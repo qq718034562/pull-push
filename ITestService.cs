ab6a2b363e55ed54fdb45a8f947ddce027b51014
qq718034562 <718034562@qq.com>
2020-02-11 15:15:25 +0800

    ITestService.cs add

---
+++ ITestService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCoreMVC
+{
+    public interface ITestServiceA
+    {
+        void Test();
+    }
+	//方法TestServiceA 一
+    public class TestServiceA : ITestServiceA
+    {
+        public void Test()
+        {
+            Console.WriteLine("123");
+        }
+    }
+
+    public class TestServiceB : ITestServiceA
+    {
+        public void Test()
+        {
+            Console.WriteLine("456");
+        }
+    }
+}
